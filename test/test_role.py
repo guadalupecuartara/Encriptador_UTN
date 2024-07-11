a78c2f9bcce5885d64eebc24bba0cde201c20183
guadalupecuartara <guadaluc08@gmail.com>
2024-07-11 19:58:49 -0300

    cargar docker-compose

--- test/test_role.py
+++ test/test_role.py
@@ -9,9 +9,7 @@ class RoleTestCase(unittest.TestCase):
         self.app = create_app()
         self.app_context = self.app.app_context()
         self.app_context.push()
-        self.ROL_NAME = 'ROLE_ADMIN'
-        self.ROL_DESCRIPCION = 'Administrator'
-
+        
     def tearDown(self):
         self.app_context.pop()
 
@@ -19,15 +17,11 @@ class RoleTestCase(unittest.TestCase):
         self.assertIsNotNone(current_app)
     
     def test_role(self):
-        role = self.__get_role()
-        self.assertTrue(role.name, 'ROLE_ADMIN')
-        self.assertTrue(role.description, 'Administrator')
-    
-    def __get_role(self) -> Role:
-        role = Role()
-        role.name = self.ROL_NAME
-        role.description = self.ROL_DESCRIPCION
-        return role
+        user = Role()
+        user.name = 'ROLE_ADMIN'
+        user.description = 'Administrator'
+        self.assertTrue(user.name, 'ROLE_ADMIN')
+        self.assertTrue(user.description, 'Administrator')
 
 if __name__ == '__main__':
     unittest.main()
